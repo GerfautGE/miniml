3f64969c46d650ba667bd28ad946971f89131fab
Luc Videau <73294402+GerfautGE@users.noreply.github.com>
2025-03-16 20:23:56 +0100

    Fix typo in parser.ml and add build profile file

--- bin/parser.ml
+++ bin/parser.ml
@@ -3,4 +3,4 @@ open Miniml.Token
 
 let parser (tks: token list) : unit =
   List.iter (fun t -> Printf.printf "%s\n" (string_of_token t)) tks;
-  Printf.printf "TODO: parsing  ot implemented yet\n"
+  Printf.printf "TODO: parsing not implemented yet\n"
