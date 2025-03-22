f185b809a1b75a262fefe96eca6ac60c9c91e9ac
Gerf <73294402+GerfautGE@users.noreply.github.com>
2025-03-22 11:09:19 +0100

    Add string conversion functions and improve AST test visualization
    This commit introduces two string conversion helper functions for 'const' and 'op' data types in 'ast.ml'. It also enhances the 'dot_of_ast' function in 'test_ast.ml' to display nodes in a more readable and meaningful way when visualizing the Abstract Syntax Tree (AST). The tree dump was updated with a more reflective example as well.

--- lib/Syntax/ast.ml
+++ lib/Syntax/ast.ml
@@ -4,9 +4,17 @@ type const =
   | Cint of int
   | Cbool of bool
 
+let string_of_const = function
+  | Cint i -> string_of_int i
+  | Cbool b -> string_of_bool b
+
 type op =
   | Add
   | Mul
+
+let string_of_op = function
+  | Add -> "+"
+  | Mul -> "*"
 
 type node_kind =
   | Tidentifier of string
--- test/test_ast.ml
+++ test/test_ast.ml
@@ -1,15 +1,42 @@
 open Miniml.Ast
 
 
-let dot_of_ast (oc: out_channel) (_: tree) : unit =
+let dot_of_ast (oc: out_channel) (root: tree) : unit =
   Printf.fprintf oc "digraph G {\n";
-  (* Node Todo *)
-  Printf.fprintf oc "Todo;\n";
+  Printf.fprintf oc "node [shape=record];\n";
+  let rec aux (node: tree) : unit =
+    match node with
+    | Node(nk, children) ->
+      let node_id = (Hashtbl.hash node) in
+      let node_label = match nk with
+        | Tidentifier s -> Printf.sprintf "Identifier %s" s
+        | Tconstant c -> Printf.sprintf "Constant %s" (string_of_const c)
+        | Top o -> Printf.sprintf "Op %s" (string_of_op o)
+        | Tanonfun -> "AnonFun"
+        | Tapply -> "Apply"
+        | Tpair -> "Pair"
+        | Tlocalbinding -> "LocalBinding"
+      in
+      Printf.fprintf oc "%d [label=\"%s\"];\n" node_id node_label;
+      List.iter (fun child ->
+          let child_id = (Hashtbl.hash child) in
+          Printf.fprintf oc "%d -> %d;\n" node_id child_id;
+          aux child
+        ) children
+  in
+  aux root;
   Printf.fprintf oc "}\n"
 
 let dump_ast: unit =
-  (* let x = 42 in fun y -> x + y 3 *)
-  let le_tree = Node(Tlocalbinding, [Node(Tidentifier "x", []); Node(Tconstant (Cint 42), []); Node(Tanonfun, [Node(Tidentifier "y", []); Node(Top Add, [Node(Tidentifier "x", []); Node(Tidentifier "y", [])]); Node(Tconstant (Cint 3), [])])]) in
+  (* let main = fun _ -> 0*)
+  let le_tree =
+    Node(Tlocalbinding, [
+      Node(Tidentifier "main", []);
+      Node(Tanonfun, [
+        Node(Tidentifier "_", []);
+        Node(Tconstant (Cint 0), [])
+      ])
+    ]) in
   let oc = open_out "/tmp/ast.dot" in
   let _ = dot_of_ast oc le_tree in
   close_out oc
